26f61ce1d0e5f0333e8130cd71f827268696e5c2
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-12-24 01:01:48 +0530

    utilis code discussion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+  // Listen for server errors
+  app.on("error", (err) => {
+    console.error("Server error:", err);
+  });
+  
+  app.listen(process.env.PORT || 8000,() => {
+    console.log(`Server is running at port : ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGO db connection failed !!!",err);
+})
 
 
 
