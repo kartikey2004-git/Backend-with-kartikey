67519e894645c4ef6a5f721c7021183de29baa89
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-05-07 21:52:31 +0530

    add routes/endpoint for changing passowrd , get current user , update account details, update avatar and coverImage , get watchHistory and channelProfile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
   updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -24,10 +30,35 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 // secured routes
+
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
 
-router.patch("/avatar", verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+// bss frontend wale ko pata hona chahiye ki kya krne wale iss route pe
+
+// patch mein resource sirf partially update hota hai wrna post mein saari details update hojayengi
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+// frontend wale ka role hai ki hum usko kya bol rhe hai and usko usi naam se bhejni padegi and hum or checks bhi laga skte hai
+
+// but ab baat aati getUserChannelProfile tab hum channel ka username params mein se le rhe hai , tab problem aati hai
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
